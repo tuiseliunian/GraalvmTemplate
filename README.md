1e8500d5bd2d3395ed40a9f4e31886796427e0f6
wentian <jefzeng816@gmail.com>
2023-11-15 21:30:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 简介
+
+使用maven插件调用build目录下compile.sh或compile.bat并通过graalvm将jar包编译为可执行文件。
+    
---
+++ src/main/java/com/zj/Main.java
@@ -0,0 +1,14 @@
+package com.zj;
+
+import java.net.*;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
